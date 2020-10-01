f2e8d550b69269e918f161d863217aee6a4241f1
fuadop <fuadolatunji@gmail.com>
2020-10-01 10:58:51 +0100

    schema update

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,14 @@
 import express from "express";
 import {graphqlHTTP} from "express-graphql";
 import {buildSchema} from "graphql";
-import rootQuery from "./schemas/rootSchema.gql";
+import Schema from "./schemas/Schema.gql";
 import Resolvers from "./schemas/resolvers";
 
 const PORT = process.env.PORT || 4000;
 const app = express();
 
 app.use("/graphql", graphqlHTTP({
-    schema: buildSchema(rootQuery),
+    schema: buildSchema(Schema),
     rootValue: Resolvers,
     graphiql: true
 }));
